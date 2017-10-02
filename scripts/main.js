5c79e5d5c04b2c864a096eace9730b2ad038041d
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-10-02 12:11:09 +0200

    Added basic graphic layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,19 @@
-let notification = document.querySelector('.notification');
 let password = '';
+let receivePassword = document.querySelector('#passwordReceiveBox');
 function generatePassword() {
   let passwordLength = document.querySelector('#passwordLength').value;
-  notification.innerHTML = '';
   if(passwordLength < 8) {
-    notification.innerHTML='<div class="alert alert-danger" role="alert">Strong password require minimum 8 characters!</div>';
+    notification.innerHTML='Strong password require minimum 8 characters!';
   }
-  else if (passwordLength > 30) {
-    notification.innerHTML='<div class="alert alert-danger" role="alert">You will not remember too long password</div>';
+  else if (passwordLength > 50) {
+    notification.innerHTML='You will not remember too long password';
   }
   else {
     for(let i=0; i<passwordLength; i++) {
         let character = 33 + Math.floor(Math.random()*92);
         password += String.fromCharCode(character);
     }
-    notification.innerHTML=password;
+
   }
 }
 
