a7ab8af256488089963e15aadf8f46eec12ff8f5
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-10-02 15:15:43 +0200

    Now we can choose characters to our password e.g. uppercase letters

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 let password = '';
 let receivedPassword = document.querySelector('#passwordReceiveBox');
 let alertBox = document.querySelector('#currentAlert');
+let uppercaseLetters = document.querySelector('#uppercaseLetters');
+let numbers = document.querySelector('#numbers');
+let specialCharacters = document.querySelector('#specialCharacters');
 function generatePassword() {
   password = '';
   let passwordLength = document.querySelector('#passwordLength').value;
@@ -20,6 +23,18 @@ function generatePassword() {
     alertBox.innerHTML = "Here's your password! Click on the input to copy your password to your clipboard";
     for(let i=0; i<passwordLength; i++) {
         let character = 33 + Math.floor(Math.random()*92);
+        if(!(uppercaseLetters.checked)&&(character > 64 && character < 91)) {
+          passwordLength++;
+          continue;
+        }
+        if(!(numbers.checked)&&(character > 47 && character < 58)) {
+          passwordLength++;
+          continue;
+        }
+        if(!(specialCharacters.checked)&&((character>32&&character<48)||(character>57&&character<65)||(character>90&&character<97)||(character>122&&character<127))) {
+          passwordLength++;
+          continue;
+        }
         password += String.fromCharCode(character);
     }
     receivedPassword.value = password;
