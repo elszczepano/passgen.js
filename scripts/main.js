ddc3c2a182ecdb10cdd63ae84840a7691882241f
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-10-02 13:03:08 +0200

    final stage

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,40 @@
 let password = '';
-let receivePassword = document.querySelector('#passwordReceiveBox');
+let receivedPassword = document.querySelector('#passwordReceiveBox');
+let alertBox = document.querySelector('#currentAlert');
 function generatePassword() {
+  password = '';
   let passwordLength = document.querySelector('#passwordLength').value;
   if(passwordLength < 8) {
-    notification.innerHTML='Strong password require minimum 8 characters!';
+    alertBox.classList.remove('text-success');
+    alertBox.classList.add('text-danger');
+    alertBox.innerHTML = "Strong password requires at least 8 characters!";
   }
   else if (passwordLength > 50) {
-    notification.innerHTML='You will not remember too long password';
+    alertBox.classList.remove('text-success');
+    alertBox.classList.add('text-danger');
+    alertBox.innerHTML = "This password is too long!";
   }
   else {
+    alertBox.classList.remove('text-danger');
+    alertBox.classList.add('text-success');
+    alertBox.innerHTML = "Here's your password! Click on the input to copy your password to your clipboard";
     for(let i=0; i<passwordLength; i++) {
         let character = 33 + Math.floor(Math.random()*92);
         password += String.fromCharCode(character);
     }
-
+    receivedPassword.value = password;
   }
 }
-
+function copyToClipboard() {
+  if( password != '')
+  {
+    receivedPassword.select();
+    document.execCommand('copy');
+    alertBox.classList.remove('text-danger');
+    alertBox.classList.add('text-success');
+    alertBox.innerHTML = "Copied!";
+  }
+}
 const generateButton = document.querySelector('#generatePassword');
 generateButton.addEventListener('click',generatePassword);
+receivedPassword.addEventListener('click',copyToClipboard);
